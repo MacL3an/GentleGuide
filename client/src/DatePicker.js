094a3efef704adde40466d6e1781f48b10b902e3
Håkan MacLean <hakan@maclean.se>
2019-02-27 21:40:47 +0100

    Turned date into text and not field input

--- client/src/DatePicker.js
+++ client/src/DatePicker.js
@@ -7,25 +7,10 @@ class DatePicker extends Component {
             date: props.date
         };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.oneDayAhead = this.oneDayAhead.bind(this);
     this.oneDayBack = this.oneDayBack.bind(this);
     }
     
-    handleChange(event) {
-        console.log('onchange');
-        this.setState({date: event.target.value});
-      }
-    
-    handleSubmit(event) {
-        console.log('submitting');
-        // this.handleChange(event);
-        console.log(this.state.date);
-        this.props.updateRecommendations(this.state.date);
-        event.preventDefault();
-    }
-
     oneDayAhead() {
         console.log("forward: " + this.state.date);
         let dateCopy = new Date(this.state.date.getTime());
@@ -43,13 +28,12 @@ class DatePicker extends Component {
     }
 
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                Currently showing recommendations based on avalanche forecast for: 
-                <button type="button" onClick={this.oneDayBack}>&larr;</button>
-                <input type="text" name="dateInput" size="12" value={this.state.date.toISOString().substr(0, 10)} onChange={this.handleChange}></input>
-                <button type="button" onClick={this.oneDayAhead}>&rarr;</button>
-            </form>
+        return (<div>
+                Currently showing recommendations based on avalanche forecast for <br/><br/>
+                    <button type="button" onClick={this.oneDayBack}>&larr;</button>
+                    &nbsp;{this.state.date.toISOString().substr(0, 10)}&nbsp;
+                    <button type="button" onClick={this.oneDayAhead}>&rarr;</button>
+                </div>
         );
     }
 }
